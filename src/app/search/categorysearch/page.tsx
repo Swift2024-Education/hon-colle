7ecd8bd212064b9317d336a0cfb530abedda13a2
kiguruu <171789840+kiguruu@users.noreply.github.com>
2024-10-25 17:22:41 +0900

    Separate Search Page into Keyword Seacrh and Category Search

---
+++ src/app/search/categorysearch/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <>
+      <div className="bg-gradient-to-b from-white to-stone-300 h-dvh">
+        <p className="text-gray-700 text-center">
+            Category Page
+        </p>
+      </div>
+    </>
+  );
+}
