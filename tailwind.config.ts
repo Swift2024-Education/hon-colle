a1a0248cb7bc3ce290d0ab97c7ea32d023a8f953
kiguruu <171789840+kiguruu@users.noreply.github.com>
2024-11-07 15:20:16 +0900

    Add: Added user-defined color as sky-swift(#D0EDF3) to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'sky-swift': '#D0EDF3',
       },
       size: {
         '128': '32rem',
