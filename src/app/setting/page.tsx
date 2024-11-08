49e0d5fca46c49dae439686837a97cfae92eb20d
Hide0926 <171790103+Hide0926@users.noreply.github.com>
2024-11-08 17:52:30 +0900

    名前を入力して下さい．を表示

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,42 +1,57 @@
-import Search from "@/app/ui/search";
+'use client'
+
 import Link from "next/link";
+import { useState, ChangeEvent } from 'react'
 
 export default function Page() {
+
+    const [inputText, setInputText] = useState('');
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputText(e.target.value);
+    };
+
+    const handleClick = () => {
+        console.log(inputText);
+    };    
+
     return (
-        <>
           <div className="bg-sky-200 h-max min-h-screen flex">
             <div className="bg-amber-300 min-h-48 h-fit my-20 mx-auto rounded-xl">
                 <div className="bg-white m-8 rounded-xl">
                     <div className="p-4">
 
-                        <h2 className="text-gray-700 pl-8 p-4 text-4xl font-bold text-center">プロフィールシート</h2>
+                        <h2 className="text-gray-700 pl-8 p-4 text-4xl font-bold text-center">せってい</h2>
 
                         <div className="bg-amber-100 rounded-full w-fit ml-8">
                             <div className="text-gray-700 text-xl text-center mx-6">なまえ</div>
                         </div>
 
-                        <input className= "pl-8 p-1 max-w-md border-stone-950 rounded-xl placeholder-gray-500 border ml-16" placeholder="鈴木 悠太"></input>
-                            {/*名前を入力*/}
-
+                        <input className= "pl-8 p-1 max-w-md border-stone-950 rounded-xl placeholder-gray-500 border ml-16" placeholder="結城 さくな" onChange={handleChange}></input>
+                            {inputText=== '' && (
+                            <p className="text-red-500 text-center italic text-base">
+                                名前を入力してください。
+                                </p>
+                            )}
+                            {/*名前を入力する場所*/}
                         <div className="bg-amber-100 rounded-full w-fit ml-8">
                             <div className="text-gray-700 text-xl text-center mx-6">ログイン</div>
                         </div>
 
                         <h3 className="text-gray-700 pl-8 p-1 text-xl">googleアカウント表示予定（仮）</h3>
 
-                        <div className="bg-orange-600 rounded-full w-fit ml-8">
-                            <div className="text-gray-700 text-xl text-center mx-6">
-                                <Link href="../">
+                        <button className="bg-orange-600 rounded-full w-fit ml-8" onClick={handleClick}>
+                            <Link href="../">
+                                <div className="text-gray-700 text-xl text-center mx-6">
                                     せってい を ほぞん
-                                </Link>
-                            </div>
-                        </div>
-                        {/*クリックでSearchに移動*/}
+                                </div>
+                            </Link>
+                        </button>
+                        {/*クリックで/Searchに移動，コンソールに入力された名前を表示*/}
                         
                     </div>
                 </div>
             </div>
           </div>
-        </>
       );
 }
