e104a048ba7071717ca52db4dac65d13f65f8f0d
kiguruu <171789840+kiguruu@users.noreply.github.com>
2024-11-15 19:58:02 +0900

    Fix: Revert prisma.ts to solve internal server error.

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,17 +1,6 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client/edge';
-import { withAccelerate } from '@prisma/extension-accelerate';
+import { PrismaClient } from "@prisma/client";
 
-const PrismaClientSingleton = () => {
-    return new PrismaClient().$extends(withAccelerate());
-};
-
-declare const globalThis: {
-    prismaGlobal: ReturnType<typeof PrismaClientSingleton>;
-}& typeof global;
-
-const prisma = globalThis.prismaGlobal ?? PrismaClientSingleton();
+const prisma = new PrismaClient();
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
